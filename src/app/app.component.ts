e830f08fc930be05cf6ffe84ca132c41b6a57279
Gianfranco Estevez Ruiz <gianestevezruiz@hotmail.com>
2025-03-20 18:51:09 -0500

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // ✅ En lugar de BrowserModule
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule, // ✅ Cambiado de BrowserModule a CommonModule
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatRippleModule,
+    FormsModule,
+    FlexLayoutModule,
+    MatSidenavModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
