62be5e12582e5599ae36ada248ffb87d534dc04e
Gianfranco Estevez Ruiz <gianestevezruiz@hotmail.com>
2025-03-24 03:32:30 -0500

    newv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-wrapper-object-types */
+import { Component, HostListener, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common'; // ✅ En lugar de BrowserModule
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,15 +9,17 @@ import { MatRippleModule } from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    CommonModule, // ✅ Cambiado de BrowserModule a CommonModule
+    CommonModule,
     MatToolbarModule,
     MatIconModule,
+    MatListModule,
     MatButtonModule,
     MatRippleModule,
     FormsModule,
@@ -26,6 +29,43 @@ import { MatSidenavModule } from '@angular/material/sidenav';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cv';
+  isExpanded = false;
+  isMobile = false;
+  isReady = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.updateScreenSize();
+    }
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        this.isReady = true;
+      });
+    }
+  }
+
+  redirectTo(url: string) {
+    window.open(url, '_blank'); 
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.updateScreenSize();
+    }
+  }
+
+  private updateScreenSize() {
+    this.isMobile = window.innerWidth < 960;
+  }
+
+  toggleMenu() {
+    this.isExpanded = !this.isExpanded;
+  }
 }
+
