bfbbdc62690f7eeaef359468a6c8d333364e4338
Gianfranco Estevez <gianestevezruiz@hotmail.com>
2026-02-07 17:10:11 -0500

    v5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-/* eslint-disable @typescript-eslint/no-wrapper-object-types */
-import { Component, HostListener, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  Inject,
+  PLATFORM_ID,
+  signal
+} from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatRippleModule } from '@angular/material/core';
-import { FormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
@@ -19,53 +21,52 @@ import { MatListModule } from '@angular/material/list';
     CommonModule,
     MatToolbarModule,
     MatIconModule,
-    MatListModule,
-    MatButtonModule,
-    MatRippleModule,
-    FormsModule,
-    FlexLayoutModule,
-    MatSidenavModule
+    MatButtonModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'cv';
-  isExpanded = false;
-  isMobile = false;
-  isReady = true;
+export class AppComponent {
+
+  readonly isExpanded = signal(false);
+  readonly isMobile = signal(false);
 
   constructor(@Inject(PLATFORM_ID) private platformId: object) {
-    if (isPlatformBrowser(this.platformId)) {
-      this.updateScreenSize();
-    }
-  }
-
-  ngOnInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      setTimeout(() => {
-        this.isReady = true;
-      });
-    }
-  }
-
-  redirectTo(url: string) {
-    window.open(url, '_blank'); 
-  }
-
-  @HostListener('window:resize')
-onResize() {
-  if (isPlatformBrowser(this.platformId)) {
     this.updateScreenSize();
   }
-}
+
+  // ---------- Navigation ----------
+
+  openGithub() {
+    window.open('https://github.com/gerufireblade', '_blank');
+  }
+
+  openLinkedIn() {
+    window.open(
+      'https://www.linkedin.com/in/gianestevez/',
+      '_blank'
+    );
+  }
+
+  // ---------- Responsive logic ----------
+
+  @HostListener('window:resize')
+  onResize() {
+    this.updateScreenSize();
+  }
 
   private updateScreenSize() {
-    this.isMobile = window.innerWidth < 960;
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    this.isMobile.set(window.innerWidth < 960);
+
+    // Close menu automatically when switching to desktop
+    if (!this.isMobile()) {
+      this.isExpanded.set(false);
+    }
   }
 
   toggleMenu() {
-    this.isExpanded = !this.isExpanded;
+    this.isExpanded.update(v => !v);
   }
 }
-
