638da100c98c1be75048fadc812e39421c0f4895
Gianfranco Estevez <gianestevezruiz@hotmail.com>
2026-02-07 17:28:10 -0500

    v7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import {
-  Component,
-  HostListener,
-  Inject,
-  PLATFORM_ID,
-  signal
+  Component
 } from '@angular/core';
 
 import { RouterOutlet } from '@angular/router';
-import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -28,15 +24,6 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class AppComponent {
 
-  readonly isExpanded = signal(false);
-  readonly isMobile = signal(false);
-
-  constructor(@Inject(PLATFORM_ID) private platformId: object) {
-    this.updateScreenSize();
-  }
-
-  // ---------- Navigation ----------
-
   openGithub() {
     window.open('https://github.com/gerufireblade', '_blank');
   }
@@ -46,27 +33,5 @@ export class AppComponent {
       'https://www.linkedin.com/in/gianestevez/',
       '_blank'
     );
-  }
-
-  // ---------- Responsive logic ----------
-
-  @HostListener('window:resize')
-  onResize() {
-    this.updateScreenSize();
-  }
-
-  private updateScreenSize() {
-    if (!isPlatformBrowser(this.platformId)) return;
-
-    this.isMobile.set(window.innerWidth < 960);
-
-    // Close menu automatically when switching to desktop
-    if (!this.isMobile()) {
-      this.isExpanded.set(false);
-    }
-  }
-
-  toggleMenu() {
-    this.isExpanded.update(v => !v);
   }
 }
